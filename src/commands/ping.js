ebc07dfd5ddac5c7202eafb7f38f9dbbda72cf94
VL07 <79533745+VL07@users.noreply.github.com>
2022-02-01 16:26:10 +0100

    Updated ping command

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,10 +1,22 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
 	command: new SlashCommandBuilder()
 		.setName("ping")
 		.setDescription("pong"),
 	async execute(interaction) {
-		await interaction.reply("Pong");
+		const embed = new MessageEmbed()
+			.setColor("2F3136")
+			.setTitle("🏓 | Ping")
+			.setDescription(`Bot latency: \`Loading...\`\nWebsocket latency: \`${interaction.client.ws.ping}ms\``)
+			.setFooter({ text: "/help" })
+			.setTimestamp();
+
+		const message = await interaction.reply({ embeds: [embed], fetchReply: true });
+
+		embed.setDescription(`Bot latency: \`${message.createdTimestamp - interaction.createdTimestamp}ms\`\nWebsocket latency: \`${interaction.client.ws.ping}ms\``);
+
+		await interaction.editReply({ embeds: [embed] });
 	},
 };
